9a2eba59d0885beb5a68a085a499576358f1e647
Ahmet Kaan Mönün <ahmetkaan.monun@outlook.com>
2022-02-10 22:57:48 +0300

    first commit

---
+++ Fundamentals-1/Values&Variables/script.js
@@ -0,0 +1,13 @@
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+
+let country = "Turkey";
+let continent = "Europe";
+let population = "80.5";
+
+// 2. Log their values to the console
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
